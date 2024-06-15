71749f1fd7450fa1054260e805d6b40e7e85ad3d
kunal29choubey <kunal29choubey.com>
2024-06-15 18:33:01 +0530

    Student Details

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,70 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [data, setData] = useState<number>(0);
-  const[numberData, setNumberData] = useState<number>(0) ;//usestate it means to change the bata
+function time() {
+  
+//   const [data, setData] = useState<number> (0);
+//   const[numberData, setNumberData] = useState<number>(0);
+  
+//  const numberArray = [1,2,3,4,5,6,7,7];
+// const people =[
+//   {name:"vishal", age:20},
+//   {name:"sunil", age:21},
+//   {name:"rahul", age: 22}
+// ]
 
-  useEffect(() => { addData();}, [numberData]);
-  const addData = ()=>{
-const oldData = data;
-const newData = oldData + 1;
-setData(newData);
-console.log(newData);
+//   useEffect(() =>{
+//     //fix: added a function to be executed when the effect is triggered
+//     console.log('Effect triggered');
+    
+//   }, [numberData]);/*{ addData();}, [numberData]);*/
+//   const addData = ()=>{
+// const oldData = data;
+// const newData = oldData + 1;
+// setData(newData);
+// console.log(newData);
 
-  //   setData("world");
-  };
-  const decrementData = () => {
-    setNumberData(numberData - 1);
-  };
-  return (
-    <>
-    <button title="title" onClick={addData} className={'number ${data}'}> add Data</button>
+    
+//   };
+//   const decrementData = () => {
+//     setNumberData(numberData - 1);
+//   };
+//   return (
+//     <>
+//     <button title="title" onClick={addData} className={'number ${data}'}> add Data</button>
 
-      <div>incremented Data: {data}</div>
-      <br />
-      <button onClick = {decrementData} className={'decrement ${data}'}> decrement</button>
-      <div>decrementData: {numberData}</div>
-      </>
-  );       
-}
+//       <div>incremented Data: {data}</div>
+//       <br />
+//       <button onClick = {decrementData} className={'decrement ${data}'}> decrement</button>
+//       <div>decrementData: {numberData}</div>
+
+//       {numberArray.map((item, i) => {
+//         return <div key={i}>{item}</div>;
+//       })}
+
+//       {people.map((item, i) => {
+//         return (
+//           <div key={i}>
+//             {item.name === "vishal" ? (
+//               <>
+//                 <div>name: {item.name}</div>
+//                 <div>Age: {item.age}</div> {/* Fix: capitalized 'Age' */}
+//                 <br />
+//               </>
+//             ) : (
+//               <>
+//                 <div>
+//                   inactive
+//                 </div>
+//                 <br />
+//               </>
+//             )}
+//             </div>
+//         );
+//       })}
+//     </>
+//   );
+// }
+
 
 export default App
